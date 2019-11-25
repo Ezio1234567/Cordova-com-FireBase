c40f2180153ede66f0e933d3789bca032f48db8e
Ezio1234567 <eziopneto@globomail.com>
2019-11-25 20:58:57 -0300

    cordova-firebase

---
+++ www/firebaseConfig.js
@@ -0,0 +1,10 @@
+var fbConfig = {
+    apiKey: "AIzaSyA-jKSnwrOhjTnktDLdVS8VAsUsxeMvLCg",
+    authDomain: "aula-cordova-cec6f.firebaseapp.com",
+    databaseURL: "https://aula-cordova-cec6f.firebaseio.com",
+    projectId: "aula-cordova-cec6f",
+    storageBucket: "aula-cordova-cec6f.appspot.com",
+    messagingSenderId: "28836584242",
+    appId: "1:28836584242:web:9bcc80f875c473740b952b",
+    measurementId: "G-RZTN4TD1N4"
+  };
